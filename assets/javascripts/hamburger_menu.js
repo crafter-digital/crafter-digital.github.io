dc53b251d2cec35b1d0191e84941ff0726ec15b9
Fatimah Az-Zahra <azzahrafz@gmail.com>
2019-08-08 13:33:56 +0700

    Fix menu to show close icon

--- assets/javascripts/hamburger_menu.js
+++ assets/javascripts/hamburger_menu.js
@@ -1,11 +1,17 @@
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
-    document.getElementById("menu-icon").addEventListener("click", function() {
+    var menuIcon = document.getElementById("menu-icon");
+    var closeIcon = document.getElementById("close-icon");
+    document.getElementById("icon").addEventListener("click", function() {
       var subMenu = document.getElementsByClassName('sub-menu')[0];
       if (subMenu.style.display == 'none' || subMenu.style.display == '') {
         subMenu.style.display = 'block';
+        menuIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
       } else {
         subMenu.style.display = 'none';
+        menuIcon.style.display = 'block';
+        closeIcon.style.display = 'none';
       }
     });
   }
