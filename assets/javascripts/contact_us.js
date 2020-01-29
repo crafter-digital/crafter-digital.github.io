75413830c23b8081a6e14dc464922c7b05a7a0ca
Fatimah Az-Zahra <azzahrafz@gmail.com>
2020-01-29 11:20:26 +0700

    Fix background when contact us popup is open

--- assets/javascripts/contact_us.js
+++ assets/javascripts/contact_us.js
@@ -1,6 +1,11 @@
 var form_popup = document.getElementById('form_popup');
 var form_popup_overlay = document.getElementsByClassName('overlay')[0];
 var email_input = document.getElementById('contact_email_input');
+var services = document.getElementsByClassName('services')[0];
+var why_choose_us = document.getElementsByClassName('why_choose_us')[0];
+var clients = document.getElementsByClassName('clients')[0];
+var testimonial = document.getElementsByClassName('testimonial')[0];
+var footerWrapper = document.getElementsByClassName('footer-wrapper')[0];
 
 function getEmail() {
   var email = document.getElementById('contact_email');
@@ -20,7 +25,14 @@ function closePopup() {
   clearForm();
   form_popup.style.display = 'none';
   form_popup_overlay.style.display = 'none';
-  document.body.style.overflow = 'unset';
+
+  if (isInLandingPage()) {
+    services.style.display = "block";
+    why_choose_us.style.display = "block";
+    clients.style.display = "grid";
+    testimonial.style.display = "grid";
+    footerWrapper.style.display = "block";
+  }
 }
 
 function closeSuccessPopup() {
@@ -36,8 +48,17 @@ function showPopup() {
     email_input.required = false;
     email_input.style.display = 'none';
   }
-  window.scrollTo(0, 0);
-  document.body.style.overflow = 'hidden';
+  if (isInLandingPage()) {
+    services.style.display = "none";
+    why_choose_us.style.display = "none";
+    clients.style.display = "none";
+    testimonial.style.display = "none";
+    footerWrapper.style.display = "none";
+  }
+}
+
+function isInLandingPage() {
+  return services && why_choose_us && clients && testimonial && footerWrapper
 }
 
 function submitFormContact() {
